fc559fd86747bb11c0554ccb617a0d62fb8aac37
Deekshith-M13 <deekshith.m2005@gmail.com>
2026-02-18 04:37:12 +0000

    3rd que v4

---
+++ test.js
@@ -0,0 +1,11 @@
+console.log("Running tests...");
+
+// Basic test for app.js
+const testResult = true;
+
+if (testResult) {
+  console.log("✓ All tests passed!");
+} else {
+  console.error("✗ Tests failed!");
+  process.exit(1);
+}
